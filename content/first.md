9119a9987426efc49be1cd40aeca86980350a9c9
pelson <pelson.pub@gmail.com>
2013-08-14 19:42:11 +0100

    Started working on the templating.

---
+++ content/first.md
@@ -0,0 +1,14 @@
+Title: My super title
+Date: 2013-12-03 10:20
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Author: Alexis Metaireau
+Summary: Short version for index and feeds
+
+This is the content of my super blog post.
+
+{% notebook simple.ipynb %}
+
+
+
---
+++ source/pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Phil Elson'
+SITENAME = u'TBD'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGIN_PATH = '../pelican-plugins'
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+
+THEME = 'pelson_just_read'
